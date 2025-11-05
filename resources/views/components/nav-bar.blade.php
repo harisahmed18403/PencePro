370e14ca0acb910b9196f45136d0fffdc5669514
harisahmed18403 <harisahmed18403@gmail.com>
2025-11-05 22:01:47 +0000

    add daisy ui, layout and nav bar

---
+++ resources/views/components/nav-bar.blade.php
@@ -0,0 +1,4 @@
+<div class="navbar bg-base-100 shadow-sm gap-2">
+    <a class="btn btn-ghost text-xl">PencePro</a>
+    <a href="/">Home</a>
+</div>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full overflow-hidden">
+
+<head data-theme="corporate">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+
+    <!-- Styles / Scripts -->
+    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @endif
+</head>
+
+<body class="flex flex-col w-full h-full overflow-hidden">
+    <x-nav-bar></x-nav-bar>
+
+    <div class="flex-1 mx-auto w-full p-6 max-w-3xl overflow-hidden">
+        @yield('content')
+    </div>
+</body>
+
+</html>
